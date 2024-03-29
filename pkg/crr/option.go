2463be374d8a3ea8bd385749a7ec4e506de7c780
lutz <lutz.klaczynski@gmx.de>
2024-03-29 12:24:12 +0100

    Complete implementation of options, including spread options.

--- pkg/crr/option.go
+++ pkg/crr/option.go
@@ -2,8 +2,8 @@
 package crr
 
 // Option is an interface for the CRR pricer. [Expiry] is meant
-// to be a getter method, [Payoff] returns the option's payoff
-// at the given price of the underlying.
+// to be a getter method for the expiry field, [Payoff] returns
+// the option's payoff at the given price of the underlying.
 type Option interface {
 	Expiry() uint
 	Payoff(price float64) float64
@@ -16,31 +16,15 @@ type EurOption struct {
 	expiry uint
 }
 
-// CallOption implements the [Option] interface and represents
-// a call option in the CRR model with expiry and strike price.
+func (v EurOption) Expiry() uint {
+	return v.expiry
+}
+
+// CallOption implements the [Option] interface for a call
+// option in the CRR model with expiry and strike price.
 type CallOption struct {
 	EurOption
 	strike float64
-}
-
-// PutOption
-type PutOption struct {
-	EurOption
-	strike float64
-}
-
-type DigitCall struct {
-	EurOption
-	strike float64
-}
-
-type DigitPut struct {
-	EurOption
-	strike float64
-}
-
-func (v EurOption) Expiry() uint {
-	return v.expiry
 }
 
 // NewCallOption returns a [CallOption] initialised by the
@@ -50,8 +34,6 @@ func NewCallOption(expiry uint, strike float64) CallOption {
 	return CallOption{EurOption{expiry}, strike}
 }
 
-// Payoff implements the [Option.Payoff] method which makes
-// the CallOption conform to the [Option] interface.
 func (c CallOption) Payoff(price float64) float64 {
 	if price > c.strike {
 		return price - c.strike
@@ -59,6 +41,15 @@ func (c CallOption) Payoff(price float64) float64 {
 	return 0.0
 }
 
+// PutOption implements the [Option] interface for a put
+// option in the CRR model with expiry and strike price.
+type PutOption struct {
+	EurOption
+	strike float64
+}
+
+// NewPutOption returns a [PutOption] with the given expiry
+// and strike price.
 func NewPutOption(expiry uint, strike float64) PutOption {
 	return PutOption{EurOption{expiry}, strike}
 }
@@ -70,6 +61,15 @@ func (p PutOption) Payoff(price float64) float64 {
 	return 0.0
 }
 
+// DigitCall implements the [Option] interface for a digital
+// call option in the CRR model with expiry and strike price.
+type DigitCall struct {
+	EurOption
+	strike float64
+}
+
+// NewDigitalCall returns a [DigitCall] with expiry and
+// strike as given.
 func NewDigitCall(expiry uint, strike float64) DigitCall {
 	return DigitCall{EurOption{expiry}, strike}
 }
@@ -81,6 +81,15 @@ func (c DigitCall) Payoff(price float64) float64 {
 	return 0.0
 }
 
+// DigitPut implements the [Option] interface for a digital
+// put option in the CRR model with expiry and strike price.
+type DigitPut struct {
+	EurOption
+	strike float64
+}
+
+// NewDigitPut returns a [DigitPut] with expiry and
+// strike as given.
 func NewDigitPut(expiry uint, strike float64) DigitPut {
 	return DigitPut{EurOption{expiry}, strike}
 }
@@ -88,6 +97,73 @@ func NewDigitPut(expiry uint, strike float64) DigitPut {
 func (p DigitPut) Payoff(price float64) float64 {
 	if price < p.strike {
 		return 1.0
+	}
+	return 0.0
+}
+
+// DoubleDigit implements the [Option] interface for a
+// double digital option.
+type DoubleDigit struct {
+	EurOption
+	strikeLo float64
+	strikeHi float64
+}
+
+// NewDoubleDigit return a [DoubleDigit] object initialised with the
+// given parameters.
+func NewDoubleDigit(expiry uint, lo float64, hi float64) DoubleDigit {
+	return DoubleDigit{EurOption{expiry}, lo, hi}
+}
+
+func (d DoubleDigit) Payoff(price float64) float64 {
+	if price >= d.strikeLo && price <= d.strikeHi {
+		return 1.0
+	}
+	return 0.0
+}
+
+// BearSpread implements the [Option] interface for a
+// bear spread option.
+type BearSpread struct {
+	EurOption
+	strikeLo float64
+	strikeHi float64
+}
+
+// NewBearSpread return a [BearSpread] object initialised with the
+// given parameters.
+func NewBearSpread(expiry uint, lo float64, hi float64) BearSpread {
+	return BearSpread{EurOption{expiry}, lo, hi}
+}
+
+func (b BearSpread) Payoff(price float64) float64 {
+	if price >= b.strikeLo && price <= b.strikeHi {
+		return b.strikeHi - price
+	} else if price < b.strikeLo {
+		return b.strikeHi - b.strikeLo
+	}
+	return 0.0
+}
+
+// BullSpread implements the [Option] interface for a
+// bear spread option.
+type BullSpread struct {
+	EurOption
+	strikeLo float64
+	strikeHi float64
+}
+
+// NewBullSpread return a [BullSpread] object initialised with the
+// given parameters.
+func NewBullSpread(expiry uint, lo float64, hi float64) BullSpread {
+	return BullSpread{EurOption{expiry}, lo, hi}
+}
+
+func (b BullSpread) Payoff(price float64) float64 {
+	if price >= b.strikeLo && price <= b.strikeHi {
+		return price - b.strikeLo
+	} else if price > b.strikeLo {
+		return b.strikeHi - b.strikeLo
 	}
 	return 0.0
 }
--- pkg/crr/option_test.go
+++ pkg/crr/option_test.go
@@ -12,7 +12,118 @@ func TestCallOption(t *testing.T) {
 		t.Errorf("expected Expiry() == %d got %v", N, call.Expiry())
 	}
 	poff := 1.1
-	if price := 104.2; math.Abs(call.Payoff(price)-poff) > 1e-5 {
+	if price := 104.2; math.Abs(call.Payoff(price)-poff) > 1e-10 {
 		t.Errorf("expected Payoff() == %f got %f", poff, call.Payoff(price))
+	}
+	poff = 0.0
+	if price := 100.0; math.Abs(call.Payoff(price)-poff) > 1e-10 {
+		t.Errorf("expected Payoff() == %f got %f", poff, call.Payoff(price))
+	}
+}
+
+func TestPutOption(t *testing.T) {
+	const N uint = 100
+	put := NewPutOption(N, 103.1)
+	if put.Expiry() != N {
+		t.Errorf("expected Expiry() == %d got %v", N, put.Expiry())
+	}
+	poff := 0.0
+	if price := 104.2; math.Abs(put.Payoff(price)-poff) > 1e-10 {
+		t.Errorf("expected Payoff() == %f got %f", poff, put.Payoff(price))
+	}
+	poff = 0.2
+	if price := 102.9; math.Abs(put.Payoff(price)-poff) > 1e-10 {
+		t.Errorf("expected Payoff() == %f got %f", poff, put.Payoff(price))
+	}
+}
+
+func TestDigitCall(t *testing.T) {
+	const N uint = 100
+	dcall := NewDigitCall(N, 103.1)
+	if dcall.Expiry() != N {
+		t.Errorf("expected Expiry() == %d got %v", N, dcall.Expiry())
+	}
+	poff := 1.0
+	if price := 104.2; math.Abs(dcall.Payoff(price)-poff) > 1e-10 {
+		t.Errorf("expected Payoff() == %f got %f", poff, dcall.Payoff(price))
+	}
+	poff = 0.0
+	if price := 100.0; math.Abs(dcall.Payoff(price)-poff) > 1e-10 {
+		t.Errorf("expected Payoff() == %f got %f", poff, dcall.Payoff(price))
+	}
+}
+
+func TestDigitPut(t *testing.T) {
+	const N uint = 100
+	dput := NewDigitPut(N, 103.1)
+	if dput.Expiry() != N {
+		t.Errorf("expected Expiry() == %d got %v", N, dput.Expiry())
+	}
+	poff := 0.0
+	if price := 104.2; math.Abs(dput.Payoff(price)-poff) > 1e-10 {
+		t.Errorf("expected Payoff() == %f got %f", poff, dput.Payoff(price))
+	}
+	poff = 1.0
+	if price := 102.9; math.Abs(dput.Payoff(price)-poff) > 1e-10 {
+		t.Errorf("expected Payoff() == %f got %f", poff, dput.Payoff(price))
+	}
+}
+
+func TestDoubleDigit(t *testing.T) {
+	const N uint = 100
+	dod := NewDoubleDigit(N, 98.2, 103.1)
+	if dod.Expiry() != N {
+		t.Errorf("expected Expiry() == %d got %v", N, dod.Expiry())
+	}
+	poff := 0.0
+	if price := 104.2; math.Abs(dod.Payoff(price)-poff) > 1e-10 {
+		t.Errorf("expected Payoff() == %f got %f", poff, dod.Payoff(price))
+	}
+	if price := 98.0; math.Abs(dod.Payoff(price)-poff) > 1e-10 {
+		t.Errorf("expected Payoff() == %f got %f", poff, dod.Payoff(price))
+	}
+	poff = 1.0
+	if price := 102.9; math.Abs(dod.Payoff(price)-poff) > 1e-10 {
+		t.Errorf("expected Payoff() == %f got %f", poff, dod.Payoff(price))
+	}
+}
+
+func TestBearSpread(t *testing.T) {
+	const N uint = 100
+	bear := NewBearSpread(N, 98.2, 103.1)
+	if bear.Expiry() != N {
+		t.Errorf("expected Expiry() == %d got %v", N, bear.Expiry())
+	}
+	poff := 0.0
+	if price := 104.2; math.Abs(bear.Payoff(price)-poff) > 1e-10 {
+		t.Errorf("expected Payoff() == %f got %f", poff, bear.Payoff(price))
+	}
+	poff = 4.9
+	if price := 98.0; math.Abs(bear.Payoff(price)-poff) > 1e-10 {
+		t.Errorf("expected Payoff() == %f got %f", poff, bear.Payoff(price))
+	}
+	poff = 0.2
+	if price := 102.9; math.Abs(bear.Payoff(price)-poff) > 1e-10 {
+		t.Errorf("expected Payoff() == %f got %f", poff, bear.Payoff(price))
+	}
+}
+
+func TestBullSpread(t *testing.T) {
+	const N uint = 100
+	bull := NewBullSpread(N, 98.2, 103.1)
+	if bull.Expiry() != N {
+		t.Errorf("expected Expiry() == %d got %v", N, bull.Expiry())
+	}
+	poff := 4.9
+	if price := 104.2; math.Abs(bull.Payoff(price)-poff) > 1e-10 {
+		t.Errorf("expected Payoff() == %f got %f", poff, bull.Payoff(price))
+	}
+	poff = 0.0
+	if price := 98.0; math.Abs(bull.Payoff(price)-poff) > 1e-10 {
+		t.Errorf("expected Payoff() == %f got %f", poff, bull.Payoff(price))
+	}
+	poff = 4.7
+	if price := 102.9; math.Abs(bull.Payoff(price)-poff) > 1e-10 {
+		t.Errorf("expected Payoff() == %f got %f", poff, bull.Payoff(price))
 	}
 }
